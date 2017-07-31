94e3b1838bc8131a85bc677627f930c7a8571cd8
Al Delcy <aldelcy2000@gmail.com>
2017-07-31 18:12:43 -0400

    fixed exam1

--- exams/exam1.py
+++ exams/exam1.py
@@ -6,9 +6,10 @@ import exam_helpers
 
 name = input('What is your name?')
 
-exam1 = {}
+# Use and scramber and put the result here
+exam = {}
 
-result = exam_helpers.answer_eval(exam1)
+result = exam_helpers.answer_eval(exam)
 
 
 
@@ -22,20 +23,20 @@ result = exam_helpers.answer_eval(exam1)
 
 print('Here are your results')
 
-max_points = 0
-for n in exam1:
-	
-	
+max_score = 0
+for k,v in exam:
+	max_score += v['points']
+
 score = 0
 for k,v in result.items():
 	print(k, ":")
 	print("Your Answer:", v['student_answer'])
 	print("Correct Answer:", v['right_answer'])
-	print("Points Gained:", v['points_gained'])
+	print("Points Gained:", float(v['points_gained']))
 	print('--')
 
-	score+=v['points_gained']
+	score+=float(v['points_gained'])
 
 print("----------")
-print("Final score: ", score)
+print("Final score: ", score, " out of ", max_score)
 
